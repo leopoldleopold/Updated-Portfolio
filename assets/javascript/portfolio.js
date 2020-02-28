836bb777b07dab87802ef6bca9fbd5a7b4ba3bfd
leopoldleopold <55920861+leopoldleopold@users.noreply.github.com>
2020-02-28 18:18:52 -0500

    Update portfolio.js

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -1,36 +1,78 @@
-$(document).ready(function() {
-    // display main
-    // hide pages
+$(document).ready(function () {
+    // hide buttons
     $(".buttons").hide();
-    $(".about").hide();
-    $(".portfolio").hide();
-    $(".contact").hide();
 
-
-    $("#start").on("click", function() {
+    // start website 
+    $("#start").on("click", function () {
         console.log("click");
         helloHide();
-        // aboutMe();
+        aboutMe();
     });
-        function helloHide() {
-            $(".hellofade").hide();           
-        }
-        // var = ""
-        // function aboutMe() {
-        //     var 
-        //     $(".snag").append("<h1>LET'S MAKE IT MORE ABOUT ME</h1"
-        //     )
-        //     $(".snag").text("hello");
-        // }
-        function 
-    // function helloHide() {
-    //     $(".hellofade").fadeOut("slow", 5000);
-    //     setTimeout(function(){ $(".hellofade").hide(); }, 200);
-    // };
-    
-    var portfolio = ""
-    
+    //hide greeting
+    function helloHide() {
+        $(".hellofade").hide();
+    }
+    // function for "About" button press
+    $("#alink").on("click", function () {
+        aboutMe();
+    });
+    // function for "About"
+    function aboutMe() {
+        console.log(letsTalk);
+        console.log(myStatement);
+        $(".snag").empty();
+        $(".buttons").show();
+        $(".snag").css({
+            "margin": "auto",
+            "margin-top": "36px",
+            "width": "350px", "line-spacing": "25px",
+            "line-height": "20px",
+            "font-size": "18px",
+            "color": "rgba(255, 255, 255, 0.206)",
+            "text-align": "center",
+            "position": "relative",
+            "display": "overflow"
+        }).append("<h1>" + letsTalk + "</h1><br>"
+            + "<p>" + myStatement + "</p>");
+    };
+    //function for "contact" button
+    $("#clink").on("click", function () {
+        contact();
+    });
+    //function for contact
+    function contact() {
+        $(".snag").empty();
+        console.log(myInfo);
+        $(".snag").css({
+            "margin": "auto",
+            "margin-top": "36px"
+        }).append(myInfo + "<p>" + email + "</p>" + "<br>" +
+            "<p>" + linkedIn + "</p>" + "<br>" + "<p><a href='" + hub +
+            "'>" + "My Git Hub" + "</a></p>");
+    };
+    // function for "portfolio" button
+    $("#plink").on("click", function () {
+        portfolio();
+    });
+    // function for portfolio
+    function portfolio() {
+        $(".snag").empty();
+        $(".snag").append("<div id='photos'><img id='tim' src='assets/images/tim.png'>"
+        + "</div>");
+    };
+    // function to empty class and stylize
+
+    // other variables
+    var letsTalk = "LET'S TALK ABOUT ME";
+    var myStatement = "Full Stack Web Developer with a background in classical music to construct"
+        + " creative user experiences with.<br><br> Recently obtained a certificate from UPENN’s coding program,"
+        + " which provided skills in HTML, CSS, Javascript, and responsive web design."
+        + " Regarded as a passionate team member, with a large interest in problem solving,"
+        + " and creating an authentic user experience.<br><br> My goal with each project is to have immersion that"
+        + " cancels out the users realization of the experience until after it has passed."
+        + " I am looking forward to assisting, as well as learning from an experience oriented team.";
+    var myInfo = "<h1>Contact</h1>";
+    var email = "Email: <a href='mailto:jleopoldacosta@gmail.com'>jleopoldacosta@gmail.com</a>";
+    var linkedIn = "LinkedIn: https://www.linkedin.com/in/justin-acosta-0b012414a/"
+    var hub = "https://github.com/leopoldleopold";
 });
-
-
-
